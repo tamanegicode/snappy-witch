5b1619b92a9222ce6a2a0466af1f97d82e98cb33
tamanegicode <153027135+tamanegicode@users.noreply.github.com>
2024-09-06 22:20:28 +0100

    Refactor game loop on snappy-witch.cpp
    Extracted the game loop into its own separate function.
    
    This is a necessary change to prepare the game for the web build.

--- snappy-witch.cpp
+++ snappy-witch.cpp
@@ -2,19 +2,28 @@
 #include "TitleScreenState.h"
 #include "GameStateManager.h"
 
+const int canvasWidth{ 320 };
+const int canvasHeight{ 180 };
+
+int windowWidth{};
+int windowHeight{};
+
+Music backgroundMusic{};
+RenderTexture renderTexture{};
+GameStateManager gameStateManager;
+
+void UpdateDrawFrame();
+
 int main()
 {
-	const int canvasWidth{ 320 };
-	const int canvasHeight{ 180 };
-
 	SetConfigFlags(FLAG_VSYNC_HINT);
 
 	InitWindow(0, 0, "Snappy Witch");
 	InitAudioDevice();
 	ToggleBorderlessWindowed();
 
-	const int windowWidth{ GetMonitorWidth(GetCurrentMonitor()) };
-	const int windowHeight{ GetMonitorHeight(GetCurrentMonitor()) };
+	windowWidth = GetMonitorWidth(GetCurrentMonitor());
+	windowHeight = GetMonitorHeight(GetCurrentMonitor());
 
 	int framerateScale = GetMonitorRefreshRate(GetCurrentMonitor()) / 60;
 
@@ -22,13 +31,11 @@ int main()
 
 	SetTargetFPS(targetFramerate);	
 
-	RenderTexture renderTexture{ LoadRenderTexture(canvasWidth, canvasHeight) };
-
-	GameStateManager gameStateManager;
+	renderTexture = LoadRenderTexture(canvasWidth, canvasHeight);	
 
 	int maxScore{ 0 };
 
-	Music backgroundMusic = LoadMusicStream("assets/sounds/bgm.mp3");
+	backgroundMusic = LoadMusicStream("assets/sounds/bgm.mp3");
 	backgroundMusic.looping = true;
 	SetMusicVolume(backgroundMusic, 0.6f);
 
@@ -38,22 +45,7 @@ int main()
 
 	while (WindowShouldClose() != true)
 	{
-		const float deltaTime{ GetFrameTime() };
-
-		UpdateMusicStream(backgroundMusic);
-
-		gameStateManager.update(deltaTime);
-
-		BeginTextureMode(renderTexture);
-		ClearBackground(GRAY);
-
-		gameStateManager.render(deltaTime);
-
-		EndTextureMode();
-
-		BeginDrawing();
-		DrawTexturePro(renderTexture.texture, Rectangle{ 0, 0, canvasWidth, -canvasHeight }, Rectangle{ 0, 0, static_cast<float>(windowWidth), static_cast<float>(windowHeight) }, Vector2{ 0, 0 }, 0.0f, WHITE);
-		EndDrawing();
+		UpdateDrawFrame();
 	}
 
 	gameStateManager.cleanStack();
@@ -63,4 +55,24 @@ int main()
 
 	CloseAudioDevice();
 	CloseWindow();
+}
+
+void UpdateDrawFrame()
+{
+	const float deltaTime{ GetFrameTime() };
+
+	UpdateMusicStream(backgroundMusic);
+
+	gameStateManager.update(deltaTime);
+
+	BeginTextureMode(renderTexture);
+	ClearBackground(GRAY);
+
+	gameStateManager.render(deltaTime);
+
+	EndTextureMode();
+
+	BeginDrawing();
+	DrawTexturePro(renderTexture.texture, Rectangle{ 0, 0, canvasWidth, -canvasHeight }, Rectangle{ 0, 0, static_cast<float>(windowWidth), static_cast<float>(windowHeight) }, Vector2{ 0, 0 }, 0.0f, WHITE);
+	EndDrawing();
 }
